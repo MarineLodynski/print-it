82a1d9b1add1a801016f177c08d0aaa5e39c28db
MarineLodynski <marine.lodynski1@gmail.com>
2024-01-09 22:28:19 +0100

    Ajout bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,4 @@
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-	const arrowL = document.querySelector('#banner .arrow_left');
-	arrowL.addEventListener('click', function(event) {
-	  console.log("Vous avez cliqué sur la flèche gauche !");
-	});
-
-	const arrowR = document.querySelector('#banner .arrow_right');
-	arrowR.addEventListener('click', function(event) {
-	  console.log("Vous avez cliqué sur la flèche droite !");
-	});
-
-   });
-
-
-
-
-
-
-
-
-
+// Tableau pour les slides
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -40,3 +17,44 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+console.log(slides);
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	//EventListeners des deux flèches
+	const arrowL = document.querySelector('#banner .arrow_left');
+	arrowL.addEventListener('click', function(event) {
+	  console.log("Vous avez cliqué sur la flèche gauche !");
+	  console.log(slides[0])
+	  console.log(slides[0].image)
+	});
+
+	const arrowR = document.querySelector('#banner .arrow_right');
+	arrowR.addEventListener('click', function(event) {
+	  console.log("Vous avez cliqué sur la flèche droite !");
+	  console.log(slides[1])
+	  console.log(slides[1].image)
+	});
+
+	//Affichage des dots
+	const dots = document.querySelector('.dots');
+	let index = 0
+	//console.log(dots)
+	function displayDots () {
+		for (let i =0; i < slides.length; i++) {
+			const dot = document.createElement("span")
+			dot.classList.add("dot");
+			dots.appendChild(dot)
+			if (i == index) {
+				dot.classList.add("dot_selected");
+			}
+		}
+	}
+	displayDots();
+
+});
+
+
+
